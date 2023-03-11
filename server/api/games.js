845f5bac13a877907de69be2728b348cf3072bf7
John Madrid <jmadr37@gmail.com>
2023-03-11 17:47:21 -0500

    Set up route for scraping game information from metacritic

---
+++ server/api/games.js
@@ -0,0 +1,69 @@
+const puppeteer = require("puppeteer");
+const router = require("express").Router();
+const { Game } = require("../db");
+let url =
+  "https://www.metacritic.com/browse/games/score/metascore/all/all/filtered";
+
+// GET /api/games
+
+//POST /api/games
+router.post("/", async (req, res, next) => {
+  try {
+    const browser = await puppeteer.launch();
+    //function for scraping game information from metacritic
+    const extractGames = async (url) => {
+      const page = await browser.newPage({ headless: false });
+      await page.goto(url);
+      console.log(`scraping ${url}`);
+      //scrape for desired data
+
+      const gamesOnPage = await page.evaluate(() => {
+        const games = [];
+        const gameTags = document.querySelectorAll(".clamp-summary-wrap");
+        gameTags.forEach((gameTag) => {
+          const title = gameTag.querySelector("a > h3").innerText;
+          const rank = gameTag.querySelector("span").innerText;
+          const score = gameTag.querySelector("div > a > div").innerText;
+          const game = {
+            title,
+            rank,
+            score,
+          };
+          games.push(game);
+        });
+        return games;
+      });
+      await page.close();
+      //use recursion to navigate to different url
+      if (
+        url ===
+        "https://www.metacritic.com/browse/games/score/metascore/all/all/filtered?page=3"
+      ) {
+        console.log("terminating scraper...");
+      } else {
+        if (
+          url ===
+          "https://www.metacritic.com/browse/games/score/metascore/all/all/filtered"
+        ) {
+          url =
+            "https://www.metacritic.com/browse/games/score/metascore/all/all/filtered?page=0";
+        }
+        const pageNumber = parseInt(url.match(/page=(\d+)$/)[1]) + 1;
+        const nextUrl = `https://www.metacritic.com/browse/games/score/metascore/all/all/filtered?page=${pageNumber}`;
+        return gamesOnPage.concat(await extractGames(nextUrl));
+      }
+    };
+    const games = await extractGames(url);
+    const updatedGames = games.slice(0, 250);
+    const newGames = await Promise.all(
+      updatedGames.map((game) => {
+        return Game.create(game);
+      })
+    );
+    await browser.close();
+    res.json(newGames);
+  } catch (err) {
+    console.error(err.message);
+    next(err);
+  }
+});
